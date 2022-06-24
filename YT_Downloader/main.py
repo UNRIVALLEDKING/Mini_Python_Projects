00ff04888aa6704124e98a71cb8eed1d147af341
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-06-24 23:39:34 +0530

    Youtube Video Downloader

--- YT_Downloader/main.py
+++ YT_Downloader/main.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 import ffmpeg
 
-SAVE_PATH = "C:/Users/adity/Downloads/pytube"
+SAVE_PATH = "C:/Users/{user}/Downloads/pytube"
 
 link = "https://youtu.be/jH1RNk8954Q"
 yt = YouTube(link)
@@ -25,12 +25,12 @@ except:
 print("merging video")
 
 
-input_video = ffmpeg.input('C:/Users/adity/Downloads/pytube/input_video.webm')
+input_video = ffmpeg.input('C:/Users/{user}/Downloads/pytube/input_video.webm')
 
-input_audio = ffmpeg.input('C:/Users/adity/Downloads/pytube/input_audio.webm')
+input_audio = ffmpeg.input('C:/Users/{user}/Downloads/pytube/input_audio.webm')
 
 ffmpeg.concat(input_video, input_audio, v=1,
-              a=1).output('C:/Users/adity/Downloads/pytube/' + title + ".mkv").run()
+              a=1).output('C:/Users/{user}/Downloads/pytube/' + title + ".mkv").run()
 
 
 print("Task Completed!")
